9a0873ef345f73f7f541ea46ea72af756b9fec17
Ivan Mendez <166038157+ivennm@users.noreply.github.com>
2026-02-06 11:21:35 -0500

    link test

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,25 @@ function App() {
     alert("temporary alert bery bery happy")
   }
 
+  function instaLink() {
+
+  }
+
   return (
     <div>
       <h1>Yes or No</h1>
       <div class="buttons">
         <button onClick={NoHandle}>No</button>
         <button onClick={YesHandle}>Yes</button>
+      </div>
+      <div>
+        <a
+        href='https://www.instagram.com/reel/DT4kS8FCg_i/?igsh=Y3JkdXhhbDBrM3A5'
+        target='_blank'
+        rel='noopener noreferrer'
+        >
+          <button>Test</button>
+        </a>
       </div>
     </div>
   )
